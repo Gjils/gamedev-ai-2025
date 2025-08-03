f9e4d40e8d77e201d87fa353632b445788e31681
alexey <alexey>
2025-08-03 16:31:55 +0300

    add node info component template

---
+++ frontend/src/Visualization/NodeInfo/NodeInfo.tsx
@@ -0,0 +1,11 @@
+import GraphNodeInterface from "../GraphNode/GraphNodeInterface";
+
+
+function NodeInfo(props: GraphNodeInterface) {
+  return (
+    <div>
+      <h2>Информация о узле</h2>
+      <p>Здесь будет отображаться информация о выбранном узле.</p>
+    </div>
+  );
+}
